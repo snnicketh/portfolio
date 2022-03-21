f62330855d6f37086b37d17d782dd8da675132b7
Dionis Markov <ddmarrrkov@outlook.com>
2022-03-21 17:40:12 +0100

    #1

---
+++ build/precache-manifest.baf2044662cd8b8666a5b8e6912e9734.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1a989857d9393cc095e25ea9bc7fe617",
+    "url": "/index.html"
+  },
+  {
+    "revision": "164dec77a326db24520f",
+    "url": "/static/css/2.451bf2e4.chunk.css"
+  },
+  {
+    "revision": "0c2620dc0d22b8f3f9f3",
+    "url": "/static/css/main.6d61f66f.chunk.css"
+  },
+  {
+    "revision": "164dec77a326db24520f",
+    "url": "/static/js/2.d9b70e1f.chunk.js"
+  },
+  {
+    "revision": "501f379103cbfcb5ff11211dd39a28a2",
+    "url": "/static/js/2.d9b70e1f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0c2620dc0d22b8f3f9f3",
+    "url": "/static/js/main.7b052006.chunk.js"
+  },
+  {
+    "revision": "57ce957808f6b5470020",
+    "url": "/static/js/runtime-main.f64e59ee.js"
+  },
+  {
+    "revision": "6b0e28cb8dfbe1481c88a85c267178cb",
+    "url": "/static/media/dionis.6b0e28cb.png"
+  },
+  {
+    "revision": "7bd39261a742fc687dfc111e8cb72d2d",
+    "url": "/static/media/kromatika.7bd39261.png"
+  },
+  {
+    "revision": "52be31441c8cf8a8a16bd7a228d428bf",
+    "url": "/static/media/man.52be3144.svg"
+  },
+  {
+    "revision": "72bae9c4ef16fa1d2c6d81773d4e4e3a",
+    "url": "/static/media/ui.72bae9c4.svg"
+  },
+  {
+    "revision": "617d0d456400754df6fc86b68b944d0f",
+    "url": "/static/media/web.617d0d45.svg"
+  }
+]);
